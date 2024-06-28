95708c49a75c10163c7cb42ea6976e31b92ae221
Jess247 <jesss8692@hotmail.com>
2024-06-28 15:43:11 +0200

    Style input and btn

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
-var socket = io();
+let socket = io()
+
+let messages = document.getElementById('messages')
+let form = document.getElementById('form')
+let input = document.getElementById('input')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    if(input.value) {
+        socket.emit('chat message', input.value)
+        input.value = ''
+    }
+})
+
+socket.on('chat message', msg => {
+    let item = document.createElement('li')
+    item.textContent = msg
+    messages.appendChild(item)
+    window.scrollTo(0, document.body.scrollHeight)
+})
