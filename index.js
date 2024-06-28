87898da2f9e6130e4d0cc036933dea37b0a11626
Jess247 <jesss8692@hotmail.com>
2024-06-28 13:26:59 +0200

    Setup sass files

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Socket } = require('engine.io')
+const express = require('express')
+const path = require('path')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const {Server} = require("socket.io")
+const io = new Server(server)
+
+
+app.use(express.static(path.join(__dirname, 'src')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+})
+
+io.on('connection', socket => {
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg)
+    })
+
+
+    // disconnect
+    // socket.on('disconnect', () => {
+    //     console.log('User disconnected')
+    // })
+})
+
+server.listen(3000, () => {
+    console.log('listening on *:3000')
+})
